5829effeb090781efdd9f598225c8e1ee917a5f4
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-10-21 16:47:44 +0300

    fix(continue drawing after mouse is having out of the canvas and went back onto it)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
 import Konva from 'konva'
-import { onMounted, ref } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
 import { RangeInput } from './shared/ui/slider';
 const canvas = ref<HTMLDivElement>()
 const cursor = ref<HTMLDivElement>()
-const container = ref<HTMLDivElement>()
+// const container = ref<HTMLDivElement>()
 const stg = ref<Konva.Stage>()
 const lyr = ref<Konva.Layer>()
 const line = ref<Konva.Line>()
+
 const isPaint = ref(false)
 
-const activeColor = ref('#fff')
+const activeColor = ref('#000')
 const strokeWidth = ref(10)
 const changeColor = (event: any) => {
   if(!event.target && !event.target.value) return
@@ -36,44 +37,76 @@ onMounted(() => {
   lyr.value = layer
   stg.value.add(layer);
 })
-const startDraw = () => {
-  if(!stg.value) return
-  stg.value.on('mousemove', () => {
-    const selectedLine = line.value
-    const pos = stg.value?.getPointerPosition()
-    if(!pos || !isPaint.value || !selectedLine) return
-    const {x,y} = pos
-    const pointers = selectedLine.points().concat([x,y])
-    selectedLine.points(pointers)
-  })
-  stg.value.on('mouseleave', stopDraw)
-}
-const stopDraw = () => {
-  if(!stg.value) return
-  isPaint.value = false
-  stg.value.off('mousemove')
-}
-onMounted(() => {
-  if(!canvas.value || !stg.value) return
-  container.value!.addEventListener('mouseleave', () => cursor.value!.style.display = 'none')
-  container.value!.addEventListener('mouseenter', () => cursor.value!.style.display = 'block')
-  document.body.addEventListener('mousemove', cursorMove)
-  stg.value.on('mousedown', () => {
-    const pos = stg.value?.getPointerPosition()
-    if(!pos) return
-    const { x, y } = pos
-    line.value = new Konva.Line({
-      points: [x, y, x, y],
+const outsidePosition = reactive({x: 0, y: 0})
+const isOutside = ref(false)
+const draw = (e: MouseEvent) => {
+  const pos = stg.value?.getPointerPosition()
+  const width = canvas.value!.getBoundingClientRect().width - 500
+  const height = canvas.value!.getBoundingClientRect().height - 500
+  const calculatedXPosition = e.offsetX - (width/2)
+  const calculatedYPosition = e.offsetY - (height/2)
+  if(!pos || !line.value) return
+  stg.value?.on('mouseleave', () => isOutside.value = true)
+  stg.value?.on('mouseenter', () => isOutside.value = false)
+
+  if(isOutside.value){
+    outsidePosition.x = calculatedXPosition
+    outsidePosition.y = calculatedYPosition
+  }
+
+  if(isOutside.value && isPaint.value) {
+    const f = pos.x > 500 ? 500 : pos.x < 0 ? 0 : pos.x
+    const t = pos.y > 500 ? 500 : pos.y < 0 ? 0 : pos.y
+    const pointers = line.value.points().concat([f,t]) // func drawLine
+    line.value.points(pointers)
+    isPaint.value = false
+  }
+
+  if(!isOutside.value && outsidePosition.x && !isPaint.value){
+    isPaint.value = true
+    line.value = new Konva.Line({ // func startLine
+      points: [outsidePosition.x, outsidePosition.y],
       stroke: activeColor.value,
       strokeWidth: strokeWidth.value,
       lineCap: 'round',
       lineJoin: 'round',
     });
-    isPaint.value = true
     lyr.value?.add(line.value)
-    startDraw()
-  })
-  stg.value?.on('mouseup', stopDraw)
+    outsidePosition.x = 0
+    outsidePosition.y = 0
+  }
+  if(!isPaint.value) return
+  const pointers = line.value.points().concat([pos.x,pos.y])
+  line.value.points(pointers)
+}
+const startDraw = () => {
+  const pos = stg.value?.getPointerPosition()
+  if(!pos) return
+  const { x, y } = pos
+  line.value = new Konva.Line({
+    points: [x, y, x, y],
+    stroke: activeColor.value,
+    strokeWidth: strokeWidth.value,
+    lineCap: 'round',
+    lineJoin: 'round',
+  });
+  isPaint.value = true
+  lyr.value?.add(line.value)
+  canvas.value!.addEventListener('mousemove', draw)
+}
+const stopDraw = () => {
+  isPaint.value = false
+  outsidePosition.x = 0
+  outsidePosition.y = 0
+  canvas.value?.removeEventListener('mousemove', draw)
+}
+onMounted(() => {
+  if(!canvas.value || !stg.value) return
+  canvas.value!.addEventListener('mouseleave', () => cursor.value!.style.display = 'none')
+  canvas.value!.addEventListener('mouseenter', () => cursor.value!.style.display = 'block')
+  document.body.addEventListener('mousemove', cursorMove)
+  stg.value.on('mousedown', startDraw)
+  canvas.value?.addEventListener('mouseup', stopDraw)
 })
 </script>
 
@@ -84,19 +117,24 @@ onMounted(() => {
       <input type="color" @change="changeColor">
       <RangeInput :min="1" :max="125" v-model:strokeWidth="strokeWidth"/>
     </div>
-    <div @click="console.log($event)" ref="container" id="container" style="width: 100%; height: 100vh; display: flex; align-items: center; justify-content: center; cursor: none;">
+    <!-- <div @click="console.log($event)" ref="container" id="container" style="width: 100%; height: 100vh; cursor: none;"> -->
       <div class="canvas" ref="canvas" id="canvas-container"></div>
-    </div>
+    <!-- </div> -->
   </div>
 </template>
 
 <style scoped>
 .canvas {
-  border: 1px solid white;
-  max-width: 500px;
+  background: white;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
+
 .cursor {
-  border: 1px solid white;
+  border: 1px solid black;
   border-radius: 100%;
   position: absolute;
   pointer-events: none;
