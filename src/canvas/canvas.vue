410d8a94927f4ac9a37556aededaa856321f813d
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-11-09 23:03:31 +0200

    feature(draw rectangle)

--- src/canvas/canvas.vue
+++ src/canvas/canvas.vue
@@ -6,10 +6,7 @@ const canvas = ref<HTMLDivElement | null>(null)
 const stage = ref<Konva.Stage>()
 const layer = ref<Konva.Layer>()
 
-const DEFAULT_COLOR = '#000000'
-const DEFAULT_SIZE = 10
-const activeColor = ref(DEFAULT_COLOR)
-const strokeWidth = ref(DEFAULT_SIZE)
+
 /**
  * TODO I need to be able to paint and draw some figures,
  *  ? how should i implement these features? 
@@ -23,13 +20,16 @@ const strokeWidth = ref(DEFAULT_SIZE)
  /**
   * !change implementation
   */
-const paint = ({
+type Shape = {
+  startDraw: () => void,
+}
+const brush = ({
   size,
   color
 }: {
   size: number,
   color: string
-}) => {
+}): Shape => {
 
   const line = ref<Konva.Line>()
   const isPaint = ref(false)
@@ -109,7 +109,6 @@ const paint = ({
     line.value.points(pointers)
   }
   const roundCoordinate = (crd: number) => {
-    //temporarly make it hardcoded(500 is width of canvas)
     if(crd >= 0 && crd <= 500) return crd
     else if(crd > 500) return 500 
     return 0
@@ -122,19 +121,86 @@ const paint = ({
     startDraw
   }
 }
+/**
+ * !reimplement
+ */
+const rect = ({
+  color
+}:{
+  color: string
+}): Shape => {
+  const outsidePosition = reactive({x: 0, y: 0})
+  const isOutside = ref(false)
+  const startPosition = reactive<{x: null | number, y: null | number}>({x: null, y: null})
+  const startDraw = () => {
+    const pos = stage.value?.getPointerPosition()
+    if(!pos) return
+    startPosition.x = pos.x
+    startPosition.y = pos.y
+    canvas.value?.addEventListener('mousemove', draw)
+    canvas.value?.addEventListener('mouseup', stopDraw)
+  }
+  const draw = (e:any) => {
+    const calculateOutsidePosition = () => {
+      const width = canvas.value!.getBoundingClientRect().width - 500
+      const height = canvas.value!.getBoundingClientRect().height - 500
+      const xPosition = e.offsetX - (width/2)
+      const yPosition = e.offsetY - (height/2)
+      return {
+        xPosition,
+        yPosition
+      }
+    }    
+    const { xPosition, yPosition } = calculateOutsidePosition()
+    stage.value?.on('mouseleave', () => isOutside.value = true)
+    stage.value?.on('mouseenter', () => isOutside.value = false)
+    if(isOutside.value){
+      outsidePosition.x = roundCoordinate(xPosition)
+      outsidePosition.y = roundCoordinate(yPosition)
+      // console.log("X: ", outsidePosition.x)
+      // console.log("Y: ", outsidePosition.y)
+    }
+  }
+  const roundCoordinate = (crd: number) => {
+    if(crd >= 0 && crd <= 500) return crd
+    else if(crd > 500) return 500 
+    return 0
+  }
+  const stopDraw = () => {
+    const pos = stage.value?.getPointerPosition()
+    const x = outsidePosition.x
+    const y = outsidePosition.y
+    if(!pos) return
+    if(x < 0 || x > 0) {
+      console.log('here x')
+    }
+    if(y < 0 || y > 0) {
+      console.log('here x')
+    }
+    const posX = x == 0 || x == 500 ? x - startPosition.x! : pos.x - startPosition.x!
+    const posY = y == 0 || y == 500 ? y - startPosition.y! : pos.y - startPosition.y!
+    const newRect = new Konva.Rect({
+      fill: color,
+      x: startPosition.x!,
+      y: startPosition.y!,
+      width: posX,
+      height: posY,
+    })
+    canvas.value?.removeEventListener('mousemove', draw)
+    layer.value?.add(newRect)
+  }
 
-
-type DrawType = 'paint' // Square, Circle, etc..
-const createDrawingFactory = (type: DrawType) => {
-  switch(type){
-    case 'paint': 
-      return paint({color: activeColor.value, size: strokeWidth.value})
-    default: 
-      throw new Error("Invalid type")
+  return {
+    startDraw
   }
 }
 
+
 const drawFactory  = () => {
+  const DEFAULT_COLOR = '#000000'
+  const DEFAULT_SIZE = 10
+  const activeColor = ref(DEFAULT_COLOR)
+  const strokeWidth = ref(DEFAULT_SIZE)
 
   const changeColor = (event: Event) => { //take color as prop
     const target = event.target as HTMLInputElement
@@ -142,15 +208,24 @@ const drawFactory  = () => {
     const color = target.value
     activeColor.value = color
   }
-
   const changeStrokeWidth = (width: number) => {
     strokeWidth.value = width
     setCursorDiameter(width)
   }
-  /**
-   *  * Here gonna be a abstract factory that choose how to draw
-   */
-  const choseDraw = createDrawingFactory('paint')
+  
+  type DrawType = 'paint' | 'rectangle' // Square, Circle, etc..
+  const createDrawingFactory = (type: DrawType) => {
+    switch(type){
+      case 'paint': 
+        return brush({color: activeColor.value, size: strokeWidth.value})
+      case 'rectangle': 
+        return rect({color: activeColor.value})
+      default: 
+        throw new Error("Invalid type")
+    }
+  }
+  const choseDraw = createDrawingFactory('rectangle')
+
   return {
     changeColor,
     changeStrokeWidth,
