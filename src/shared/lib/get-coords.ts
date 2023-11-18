2146ef90a8cd59d634205480cd54170017e209df
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-11-18 15:25:01 +0200

    fix(when add element on the screen coords calculated wrong, made it versatile)

--- src/shared/lib/get-coords.ts
+++ src/shared/lib/get-coords.ts
@@ -1,6 +1,8 @@
 export const getCoords = (e: MouseEvent, element: HTMLElement) => {
-  const x = e.clientX - (element.getBoundingClientRect().width - 500) / 2
-  const y = e.clientY - (element.getBoundingClientRect().height - 500) / 2
+  const top = element.getBoundingClientRect().top!
+  const left = element.getBoundingClientRect().left!
+  const x = e.clientX - left
+  const y = e.clientY - top
   return {
     x,
     y
