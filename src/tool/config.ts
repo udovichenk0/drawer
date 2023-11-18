7af0d3145aaf351ecbef68bcbff18abb37f317eb
Denis Udovichenko <112856770+kkkkhe@users.noreply.github.com>
2023-11-18 21:27:38 +0200

    feature(show outline of a shape while drawing)

---
+++ src/tool/config.ts
@@ -0,0 +1,1 @@
+export const DEFAULT_PREVIEW_COLOR = '#000000'
--- src/tool/rectangle/rect.ts
+++ src/tool/rectangle/rect.ts
@@ -1,19 +1,26 @@
+import { DEFAULT_PREVIEW_COLOR } from './../config';
 import { isCanvas } from './../../shared/lib/is-canvas';
 import Konva from "konva"
 import { reactive } from "vue"
-import { isNull } from '@/shared/lib/is-null'
 import { Coordinate, Tool } from "../types"
 import { stage } from "@/canvas/viewport"
 import { getCoords } from "@/shared/lib/get-coords"
-import { normilizeCoord } from "@/shared/lib/normilize-coord"
 import { color } from "@/palette"
 
 export const rect = (): Tool => {
   const endPosition = reactive<Coordinate>({x:null, y:null})
   const startPosition = reactive<Coordinate>({x:null, y:null})
+  let rectangle: Konva.Rect
   const startDraw = (e: MouseEvent) => {
     if(e.button == 0 && isCanvas(e.target)) {
       const { x, y } = getCoords(e, getCanvas()!)
+      rectangle = new Konva.Rect({
+        stroke: DEFAULT_PREVIEW_COLOR,
+        strokeWidth: 1,
+        x,
+        y,
+      })
+      getActiveLayer()?.add(rectangle)
       startPosition.x = x
       startPosition.y = y
       trackEvents()
@@ -21,21 +28,14 @@ export const rect = (): Tool => {
   }
   const draw = (e:any) => {
     const { x, y } = getCoords(e, getCanvas()!)
+    rectangle.width(x - startPosition.x!)
+    rectangle.height(y - startPosition.y!)
     endPosition.x = x
     endPosition.y = y
   }
   const stopDraw = () => {
-    if(isNull(endPosition.x) || isNull(endPosition.y)) return
-    const rectWidth = normilizeCoord(endPosition.x!) - startPosition.x!
-    const rectHeight = normilizeCoord(endPosition.y!) - startPosition.y!
-    const newRect = new Konva.Rect({
-      fill: color.value,
-      x: startPosition.x!,
-      y: startPosition.y!,
-      width: rectWidth,
-      height: rectHeight,
-    })
-    getActiveLayer()?.add(newRect)
+    rectangle.fill(color.value)
+    rectangle.opacity(1)
     resetPosition()
     untrackEvents()
   }
